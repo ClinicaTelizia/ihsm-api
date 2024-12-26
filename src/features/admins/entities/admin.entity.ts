4e0207397fe6cddca8c2be56421852db7b6295cd
Henrique Serafim <projetoteliziaprado@gmail.com>
2024-12-26 20:06:15 -0300

    resolvendo admin

--- src/features/admins/entities/admin.entity.ts
+++ src/features/admins/entities/admin.entity.ts
@@ -1,1 +1,19 @@
-export class Admin {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('admins')
+export class Admin {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 100 })
+  nome: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  senha: string;
+
+  @Column({ default: true })
+  Atividade: boolean;
+}
